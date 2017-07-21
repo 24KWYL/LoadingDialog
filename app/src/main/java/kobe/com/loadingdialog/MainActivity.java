a1bb37af5d27740cadd950a65676de4b3eb09bf3
KOBE <wangyuelong@kmf.com>
2017-07-21 15:39:21 +0800

    no message

--- app/src/main/java/kobe/com/loadingdialog/MainActivity.java
+++ app/src/main/java/kobe/com/loadingdialog/MainActivity.java
@@ -1,7 +1,15 @@
 package kobe.com.loadingdialog;
 
+import android.provider.Settings;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +17,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
--- app/src/main/java/kobe/com/loadingdialog/RoundProgressBar.java
+++ app/src/main/java/kobe/com/loadingdialog/RoundProgressBar.java
@@ -13,7 +13,6 @@ import android.view.View;
  * Created by KOBE on 2017/7/19.
  * 自定义进度条
  */
-
 public class RoundProgressBar extends View {
 
     private Paint paint;//画笔
@@ -23,26 +22,26 @@ public class RoundProgressBar extends View {
     private int max;//最大进度
     private int progress;//当前进度
 
-    private int startAngle=-90;//起始角度
-    private int endAngle=120;
+    private int startAngle = -90;//起始角度
+    private int endAngle = 120;
 
     public RoundProgressBar(Context context) {
         super(context);
     }
 
     public RoundProgressBar(Context context, AttributeSet attrs) {
-        this(context,attrs,0);
+        this(context, attrs, 0);
     }
 
     public RoundProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        paint=new Paint();
-        TypedArray mTypedArray=context.obtainStyledAttributes(attrs,R.styleable.ProgressBar);
+        paint = new Paint();
+        TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.ProgressBar);
         //获取自定义属性和设置默认值
-        roudColor=mTypedArray.getColor(R.styleable.ProgressBar_roundColor, Color.GRAY);
-        rounddProgressColor=mTypedArray.getColor(R.styleable.ProgressBar_roundColor,Color.RED);
-        roundWidth=mTypedArray.getDimension(R.styleable.ProgressBar_roundWidth,30);
-        max=mTypedArray.getInteger(R.styleable.ProgressBar_max,80);
+        roudColor = mTypedArray.getColor(R.styleable.ProgressBar_roundColor, Color.GRAY);
+        rounddProgressColor = mTypedArray.getColor(R.styleable.ProgressBar_roundColor, Color.RED);
+        roundWidth = mTypedArray.getDimension(R.styleable.ProgressBar_roundWidth, 30);
+        max = mTypedArray.getInteger(R.styleable.ProgressBar_max, 80);
         mTypedArray.recycle();
     }
 
@@ -50,21 +49,21 @@ public class RoundProgressBar extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //画外层圆形
-        int center=getWidth()/2;//圆心坐标
-        int radius= (int) (center-roundWidth);
+        int center = getWidth() / 2;//圆心坐标
+        int radius = (int) (center - roundWidth);
         paint.setColor(roudColor);//设置颜色
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(roundWidth);//宽度
         paint.setAntiAlias(true);//消除锯齿
-        canvas.drawCircle(center,center,radius,paint);//画圆
+        canvas.drawCircle(center, center, radius, paint);//画圆
         //画进度
         paint.setColor(rounddProgressColor);
         RectF oval = new RectF(center - radius, center - radius, center
                 + radius, center + radius);  //用于定义的圆弧的形状和大小的界限
-        canvas.drawArc(oval,startAngle,endAngle,false,paint);
-        startAngle+=5;
-        if(startAngle>360){
-            startAngle-=360;
+        canvas.drawArc(oval, startAngle, endAngle, false, paint);
+        startAngle += 5;
+        if (startAngle > 360) {
+            startAngle -= 360;
         }
         invalidate();
     }
